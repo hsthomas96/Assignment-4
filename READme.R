583159a09c472402e5cd02dc2ab2136ac8a55207
Hannah Thomas <hannahthomas@live.ca>
2025-06-02 10:08:55 -0700

    Add files via upload

---
+++ READme.R
@@ -0,0 +1,9 @@
+##READme
+
+##This is a study evaluating sex-specific differences in cardiac procedures and inpatient costs within a tertiary-level hospital in northern California.
+##The total dataset contains 5000 observations and 5 variables- age, sex, smoking history, cardiac procedure and cost.
+##Files are as follows:
+##hrp203-final-1.Rmd is the main analysis and Quarto file
+##references.bib is references
+##install.packages.R is packages
+##preamble.tex is formatting settings
---
+++ install packages.R
@@ -0,0 +1,9 @@
+
+install.packages("tidyverse") 
+install.packages("palmerpenguins") 
+install.packages("table1") 
+install.packages("readr") 
+install.packages("ggplot2") 
+install.packages("Hmisc") 
+install.packages("broom") 
+install.packages("knitr")
